a1c567d62f1eaa9fd24529c452921e195ae2f22a
Jhon-Cortez <cortezvargasjhon@gmail.com>
2026-09-16 16:08:44 -0500

    refactor(profile): add data layer and service for profile persistence

---
+++ Web/Front-End/src/modules/profile/data/defaultProfile.js
@@ -0,0 +1,8 @@
+export const DEFAULT_PROFILE = {
+  fullName: 'Maria de los Angeles Olaya Garcia',
+  email: 'mariadelosangelesolayagar@gmail.com',
+  title: 'Product Manager',
+  phone: '+57 322 9523486',
+  location: 'Neiva, Colombia',
+  avatar: null,
+};
